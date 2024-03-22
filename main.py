5b977a8ac412d55812bf5742ac9b228fbf2430d0
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-22 11:32:46 +0100

    Add integration of graphql

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import strawberry
+from strawberry.asgi import GraphQL
+
 
 app = FastAPI()
 
@@ -25,3 +28,27 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_price": item.price, "item_id": item_id}
+
+
+# GraphQL
+
+
+@strawberry.type
+class User:
+    name: str
+    age: int
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def user(self) -> User:
+        return User(name="Patrick", age=100)
+
+
+schema = strawberry.Schema(query=Query)
+
+graphql_app = GraphQL(schema)
+
+app.add_route("/graphql", graphql_app)
+app.add_websocket_route("/graphql", graphql_app)
